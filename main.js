2548bfe9e06e3b0ed5ace35754405751e2210d57
NektoUa <53370133+NektoUa@users.noreply.github.com>
2020-04-04 15:34:40 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.header').height($(window).height());
+
+    // плавное перемещение 
+    $(".navbar a").click(function () {
+        $("body,html").animate({
+            scrollTop: $("#" + $(this).data('value')).offset().top
+        }, 1000)
+
+    })
+})
