ceb2691217f997b9ef8306245c22c18fe7e90ab7
NektoUa <illyabarkov@gmail.com>
2020-04-07 13:21:40 +0300

    сделан реверс в навбаре

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-$(document).ready(function () {
-    $('.header').height($(window).height());
+let a = document.querySelectorAll('.nav-link');
+function mouseIn(value) {
+    for (let nav of a) {
+        if (nav.firstChild.data == value.originalTarget.firstChild.data) {
+            let b = Array.from(nav.textContent);
+            b.reverse();
+            nav.classList.add('active-source');
+            document.querySelector('.active-source').textContent = b.join('');
+        }
+    }
+}
 
-    // плавное перемещение 
-    $(".navbar a").click(function () {
-        $("body,html").animate({
-            scrollTop: $("#" + $(this).data('value')).offset().top
-        }, 1000)
+function mouseOut(value) {
+    for (let nav of a) {
+        if (nav.firstChild.data == value.originalTarget.firstChild.data) {
+            let b = Array.from(nav.textContent);
+            b.reverse();
+            document.querySelector('.active-source').textContent = b.join('');
+            nav.classList.remove('active-source');
+        }
+    }
+}
 
-    })
-})
+document.querySelector('.navbar-nav').addEventListener('mouseover', mouseIn);
+document.querySelector('.navbar-nav').addEventListener('mouseout', mouseOut);
--- script.js
+++ script.js
@@ -18,4 +18,17 @@ $(document).ready(function () {
             $('body,html').animate({ scrollTop: 0 }, 700);
         });
     });
+
+    $('.header').height($(window).height());
+
+    // плавное перемещение 
+    $(".navbar a").click(function () {
+        $("body,html").animate({
+            scrollTop: $("#" + $(this).data('value')).offset().top
+        }, 1000)
+
+    })
 });
+
+
+
